4c0852fbe862df1e4fa998304a8c16fc95ee7cd9
Ashwini Patil <ashwinipatil1404@gmail.com>
2020-03-27 15:29:00 +0530

    replcing default stuff

--- src/containers/Sidebar/SidebarLink/SidebarLink.js
+++ src/containers/Sidebar/SidebarLink/SidebarLink.js
@@ -3,9 +3,10 @@ import styles from './SidebarLink.module.scss';
 
 const SidebarLink = (props) =>{
     const scrolllink = props.Link.toString().toLowerCase();
-    //const isActive = location.pathname.match(/^\/detail/) ? "active" : "";
+    const isActive = window.location.hash === `#` + scrolllink;
+    console.log(`#` + scrolllink);
     return(
-    <li className={styles["side-link"]}>
+    <li className={`${styles["side-link"]} ${isActive ? "active" : 'null'}`}>
         <a href={`#${scrolllink}`}>
             {props.Link}
         </a>        
